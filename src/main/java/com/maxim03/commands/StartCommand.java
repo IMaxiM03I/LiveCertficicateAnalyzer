f49066cd6704c98d0939fd9d2daf2b6cf180b45d
IMaxiM03I <maxilea202@gmail.com>
2024-03-22 23:28:04 +0100

    created 'start' command to establish connection using picocli

--- src/main/java/com/maxim03/commands/StartCommand.java
+++ src/main/java/com/maxim03/commands/StartCommand.java
@@ -1,2 +1,15 @@
-package com.maxim03.commands;public class StartCommand {
+package com.maxim03.commands;
+
+import com.maxim03.CertStreamConnection;
+
+import picocli.CommandLine.Command;
+
+@Command(name = "start", description = "Establish connection to CaliDog's CertStream server")
+public class StartCommand implements Runnable {
+
+    @Override
+    public void run() {
+        CertStreamConnection.connect();
+    }
+
 }
