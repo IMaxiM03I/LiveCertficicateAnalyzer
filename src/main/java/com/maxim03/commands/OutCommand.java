30957e7073e2906e4053908a6b4182c7ecaea070
IMaxiM03I <maxilea202@gmail.com>
2024-03-23 18:45:45 +0100

    created command to output incoming certificates to terminal

--- src/main/java/com/maxim03/commands/OutCommand.java
+++ src/main/java/com/maxim03/commands/OutCommand.java
@@ -1,2 +1,17 @@
-package com.maxim03.commands;public class OutCommand {
+package com.maxim03.commands;
+
+import com.maxim03.CertStreamConnection;
+import org.jboss.logging.Logger;
+import picocli.CommandLine.Command;
+
+@Command(name = "out", description = "Print all incoming certificates to the terminal.")
+public class OutCommand implements Runnable {
+
+    private final Logger LOG = Logger.getLogger(OutCommand.class);
+
+    @Override
+    public void run() {
+        LOG.info(CertStreamConnection.getLatestMsg());
+    }
+
 }
